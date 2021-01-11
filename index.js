313636a8ca3501dd6516ba44a6e35dba075a9e2d
Hongjung Kim <hongjung.kim.dev.gmail.com>
2021-01-11 23:41:14 +0900

    if title is clicked, it's color is red if resized, print event log

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// const title = document.getElementById("title");
-
-// console.log(title);
-
-// console.error("hello");
-// title.innerHTML = "Hi! From JS";
-
-//const title = document.getElementById("title");
-// const title = document.querySelector("#title");
-
-// title.innerHTML = "hello js";
-// title.style.color = "red";
-// console.dir(document);
-
 const title = document.querySelector("#title");
 title.innerHTML ="hello";
 title.style.color = "blue";
@@ -27,21 +13,3 @@ function handleClick(){
 
 window.addEventListener("resize", handleResize);
 title.addEventListener("click", handleClick);
-
-// function handleResize(event){
-//     console.log(event);
-// }
-
-// function handleClick(){
-//     title.style.color = "red";
-// }
-// window.addEventListener("resize", handleResize);
-// title.addEventListener("click", handleClick);
-
-// const age = prompt("How old are you");
-// if(age > 18){
-//     console.log('you can drink');
-// } else {
-//     console.log('you cant');
-// }
-
